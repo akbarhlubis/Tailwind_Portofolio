502f0dcfb1a3563cf98befbad7f6bdc7875be690
akbarhlubis <alubis87@gmail.com>
2022-10-03 13:53:39 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    container:{
+      center:true,
+      padding:'16px'
+    },
+    extend: {
+      colors:{
+        primary:'#f59e0b',
+        dark:'#0f172a'
+      },
+      screens:{
+        '2xl':'1320p'
+      }
+    },
+  },
+  plugins: [],
+}
